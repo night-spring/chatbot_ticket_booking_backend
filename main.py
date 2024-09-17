496d58fb33f3b4493e222f5b7a4883c2f84b35a8
Ayush Agarwal <dearayush2@gmail.com>
2024-09-17 19:53:32 +0530

    .

--- main.py
+++ main.py
@@ -783,7 +783,7 @@ async def handle_reserve_tickets(body, background_tasks: BackgroundTasks):
         id="66e561e683e976b3c870f802"
     else:
         id="66e561e483e976b3c870f7fe"
-    result = await payment_collection.insert_one(payment_details.dict())
+    result = await payment_collection.insert_one(PaymentDetails.dict())
     event_id = ObjectId(PaymentDetails.eventId)
     event = await shows_collections.find_one({"_id": event_id})
     background_tasks.add_task(send_email, email, event, ticket)
